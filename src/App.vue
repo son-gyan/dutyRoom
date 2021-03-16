d26a73de6ec309e84eafe26f88497eeeb13d8aa0
son_gyan <oncwnuFJFG7-Cl4qPOsb3lFMAUpc@git.weixin.qq.com>
2021-03-16 11:53:13 +0800

    ys

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<div id="app">
+		<keep-alive>
+            <router-view class="child-view" v-if="$route.meta.keepAlive"/>
+        </keep-alive>
+        <router-view class="child-view" v-if="!$route.meta.keepAlive"/>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	export default {
+		name: "App",
+		created () { 
+			//在页面加载时读取sessionStorage里的状态信息 
+			if (sessionStorage.getItem("store") ) { 
+				this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
+			} 
+			//在页面刷新时将vuex里的信息保存到sessionStorage里 
+			window.addEventListener("beforeunload",()=>{ 
+				sessionStorage.setItem("store",JSON.stringify(this.$store.state)) 
+			})
+		},
+	};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style  lang="scss">
+	html,body {
+		width: 100%;
+		height: 100%;
+	}
+	*{
+		margin:0;
+		padding: 0;
+	}
 </style>
